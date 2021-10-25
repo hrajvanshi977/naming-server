7f4f07658e0f3fe5fe118f7474739e0303ad7574
Himanshu Rajvanshi <himanshu.rajvanshi@synoriq.in>
2021-10-25 14:21:24 +0530

    commit

--- src/main/java/com/example/namingserver/NamingServerApplication.java
+++ src/main/java/com/example/namingserver/NamingServerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class NamingServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(NamingServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NamingServerApplication.class, args);
+    }
 }
